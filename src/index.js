487ce9354e891e27bdf7b9d66877c2199463002d
lulusadowsky <sadowska.luiza@gmail.com>
2020-08-12 18:01:44 +0200

    Healthbar canvas and function

--- src/index.js
+++ src/index.js
@@ -183,3 +183,26 @@ game.run();
  *
  *
  */
+
+  let display = document.getElementsById('display').getContext('2d');
+
+ 
+  
+ function drawHealthbar(canvas, x, y, width, height, health, max_health) {
+   if(health >= max_health) {health = max_health;}
+   if(health <= 0) {health = 0;}
+   canvas.fillStyle = '#000000';
+   canvas.fillRect(x, y, width, height);
+   let colorNumber = Math.round(1-(health/max_health)*0xff)*0x10000 + Math.round((health/max_health)*0xff)*0x100;
+   let colorString = colorNumber.toString(16);
+   if (colorNumber >= 0x100000) {
+     canvas.fillStyle = '#' + colorString;
+   } else if (colorNumber << 0x100000 && colorNumber >= 0x10000) {
+    canvas.fillStyle = '#0' + colorString;
+   } else if (colorNumber << 0x10000) {
+    canvas.fillStyle = '#00' + colorString;
+   }
+   canvas.fillRect(x+1, y+1, (health/max_health)*(width-2), height-2);
+ }
+
+ drawHealthbar(display, 10, 10, 300, 50, 100, 100);
